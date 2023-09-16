62dd52a5f704b8235b57e22eceabbb40a4b1453d
Sviatlana Buksa <sviatlanabuksa@gmail.com>
2023-09-16 11:59:40 +0200

    Add functionality with js. Complete the project

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
+"use strict";
 
+let barsBtn = document.querySelector(".bars");
+let cancelBtn = document.querySelector(".cancel-btn");
+let sidebar = document.querySelector("aside");
+
+barsBtn.addEventListener("click", () =>
+  sidebar.classList.toggle("show-sidebar")
+);
+
+cancelBtn.addEventListener("click", () =>
+  sidebar.classList.remove("show-sidebar")
+);
